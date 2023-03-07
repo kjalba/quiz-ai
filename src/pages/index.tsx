9e10e2015ae00df174f84eeee847d3a7234d5e86
kjalba <albakevinjulian@gmail.com>
2023-03-06 20:42:54 -0600

    Updated UI putting answer options into 4 corners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,15 +18,25 @@ const Home: NextPage = () => {
     <div className="flex h-screen w-screen flex-col justify-center items-center">
       <div className="text-2xl text-center">Who was 16th U.S President?</div>
       <div className="p-2" />
-      <div className="border rounded p-48 flex justify-between items-center max-w-4xl">
-        <div className="p-8">George</div>
-        <div className="p-8">Abe</div>
-        <div className="p-8">Bush</div>
-        <div className="p-8">Adams</div>
+      <div className="border rounded p-10 min-h-1/2 max-w-4xl">
+        <div className="grid grid-cols-2 grid-rows-2 h-full">
+          <div className="flex items-start justify-start p-4 pt-0 pl-0">
+            <div className="text-2xl">George Washington</div>
+          </div>
+          <div className="flex items-center justify-center p-4 pt-0 pl-8 pr-8">
+            <div className="text-2xl">Abraham Lincoln</div>
+          </div>
+          <div className="flex items-start justify-start p-4 pt-8 pl-0">
+            <div className="text-2xl">George H. W. Bush</div>
+          </div>
+          <div className="flex items-center justify-center p-4 pt-8 pl-8 pr-8">
+            <div className="text-2xl">John Quincy Adams</div>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Home;
 
